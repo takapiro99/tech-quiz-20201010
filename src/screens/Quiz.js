58fc7fad25cee29234f6f1046b072edad2ca003b
takapiro99 <55702777+takapiro99@users.noreply.github.com>
2020-10-10 15:25:12 +0900

    routing done!

---
+++ src/screens/Quiz.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Quiz = () => {
+  return (
+    <div>
+      <h2>クイズだよ！</h2>
+      <Link to="/">topに戻る</Link>
+    </div>
+  );
+};
+
+export default Quiz;
